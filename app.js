733cc5d7ac29664ee412d234c6df4b7ca853ce6d
Dustin Preuss <dstnpreuss@gmail.com>
2020-05-16 18:14:43 -0700

    Styling pass

--- app.js
+++ app.js
@@ -4,10 +4,17 @@ const ClientActions = {
   SuggestSong: 11,
   VoteYay: 12,
   VoteNay: 13,
+  VoteFire: 14,
+  PlaybackDone: 15,
 }
 
-const speak = (message) => {
-  speechSynthesis.speak(new SpeechSynthesisUtterance(message));
+const speak = (message, callback) => {
+  const utterance = new SpeechSynthesisUtterance(message);
+  if (callback) {
+    utterance.addEventListener("end", callback);
+  }
+
+  speechSynthesis.speak(utterance);
 }
 
 let ytReady = false;
@@ -50,35 +57,58 @@ class App extends Component {
   }
 
   render() {
-    return [
-      h('h1', null, 'Welcome to the Democratic Dance Party'),
-      h(SongSuggestion, { title: this.state.currentSuggestion, thumbnail: this.state.currentSuggestionThumbnail }),
-      h(SubmitSong, { ws: this.ws }),
-      h(BallotBox, { ws: this.ws, voteDeadline: this.state.voteDeadline }),
-      h(Player, { videoIDToPlay: this.state.videoIDToPlay }),
-    ];
+    return h('div', { class: 'main' },
+      h('h1', { key: 'header' }, 'Welcome to the Democratic Dance Party'),
+      h(SongSuggestion, { key: 'suggestion', title: this.state.currentSuggestion, thumbnail: this.state.currentSuggestionThumbnail }),
+      h(SubmitSong, { key: 'submit', ws: this.ws, currentSuggestion: this.state.currentSuggestion, videoIDToPlay: this.state.videoIDToPlay }),
+      h(BallotBox, { key: 'ballot', ws: this.ws, voteDeadline: this.state.voteDeadline }),
+      h(Player, { key: 'player', videoIDToPlay: this.state.videoIDToPlay }),
+    );
   }
 }
 
  class Player extends Component {
    componentDidUpdate(prevProps) {
-     if (prevProps.videoIDToPlay !== this.props.videoIDToPlay && this.props.videoIDToPlay !== "" && ytReady) {
-       speak("Vote has passed. Enjoy your democratically appointed jam.");
-       this.player = new YT.Player('player', {
-         playerVars: { 'controls': 0, 'autoplay': 1 },
-         height: '390',
-         width: '640',
-         videoId: this.props.videoIDToPlay,
-         events: {
-           // 'onReady': (event) => { event.target.playVideo(); },
-           //'onStateChange': onPlayerStateChange
-         }
+     debugger;
+     console.log("didUpdate", prevProps, this.props, ytReady);
+     if (prevProps.videoIDToPlay !== this.props.videoIDToPlay && this.props.videoIDToPlay !== "") {
+       debugger;
+       const initPlayer = function(autoplay) {
+         debugger;
+         this.player = new YT.Player('player', {
+           playerVars: { 'controls': 0, autoplay: autoplay },
+           height: '390',
+           width: '640',
+           videoId: this.props.videoIDToPlay,
+           events: {
+             // 'onReady': (event) => { event.target.playVideo(); },
+             //'onStateChange': onPlayerStateChange
+           }
+         });
+       }.bind(this);
+
+       if (ytReady) { initPlayer(0); }
+       else {
+         setTimeout(function() {
+           debugger;
+           initPlayer(1);
+           debugger;
+         }.bind(this), 1000);
+       }
+
+       speak("Vote has passed. Enjoy your democratically appointed jam.", () => {
+         this.player.playVideo();
        });
      }
    }
 
    render() {
-     return h('div', { id: 'player' });
+     if (this.props.videoIDToPlay === "") { return null; }
+
+     return h('div', { class: 'player-layout' },
+       h('div', { key: 'player-container', class: 'player-container' }, h('div', { id: 'player' })),
+       h('button', { class: 'fire-button' }, '🔥'),
+     );
    }
  }
 
@@ -97,10 +127,17 @@ class SubmitSong extends Component {
   }
 
   render() {
+    if (this.props.currentSuggestion !== "" || this.props.videoIDToPlay !== "") { return null; }
+
     return h('div', null,
-      h('input', { type: 'text', value: this.state.videoID, onChange: this.onChange }),
-      h('button', { onClick: this.onSubmit }, 'submit'),
-    )
+      h('div', { class: 'submit-cta'},
+        h('img', { class: 'vote-icon', src: 'vote.svg' })
+      ),
+      h('div', { class: 'submit-song-form' },
+        h('input', { type: 'text', value: this.state.videoID, onChange: this.onChange }),
+        h('button', { onClick: this.onSubmit }, 'submit'),
+      )
+    );
   }
 }
 
@@ -114,7 +151,7 @@ class SongSuggestion extends Component {
   render() {
     if (this.props.title === '' || this.props.thumbnail === '') { return null; }
 
-    return h('div', null,
+    return h('div', { class: 'song-preview' },
       h('img', { src: this.props.thumbnail, class: 'thumbnail' }),
       h('h4', null, this.props.title),
     );
@@ -135,11 +172,11 @@ class BallotBox extends Component {
     return h('div', null,
       h('button', { onClick: this.voteYay }, "Yay"),
       h('button', { onClick: this.voteNay }, "Nay"),
-  )
+    );
   }
 }
 
-render(h(App), document.body);
+render(h(App), document.querySelector("#app"));
 
 let player;
 function onYouTubeIframeAPIReady() {
